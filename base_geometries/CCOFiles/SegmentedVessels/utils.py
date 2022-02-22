cc6794c92e08ef7bdd8beecde504a563089cc8f6
remihndz <heremi@orange.fr>
2022-02-22 13:27:44 +0000

    Created a directory to experiment with multi-stages growth. Goal is to have the segmented vessels branching 'homogeneously', i.e. not only one sprouting and the others not.

---
+++ base_geometries/CCOFiles/SegmentedVessels/utils.py
@@ -0,0 +1,166 @@
+from operator import itemgetter
+
+def CreateRootTree(FOV, Radius, n):
+
+    root_nodes = [(-2.0*FOV, 0.000, 0.000), (-0.75*FOV, 0.000, 0.000)] # Root segment
+    RootConn = []                              # Connectivity for root tree
+    RootData = []
+
+    ## Define the points that will be linked to the segmented vessels
+    n = 8                           # Number of nodes along each sides of the square
+    h = 1.3*FOV/n
+    x,y = root_nodes[1][:-1]        # Point of the bifurcation
+    
+    ### The branch going above the square
+    nNodesAboveBranch = 0
+    nn = int(n/2)
+    for i in range(nn):
+        y+=h
+        root_nodes.append((x,y,0.000))
+        nNodesAboveBranch+=1
+    for i in range(n+1):
+        x+=h
+        root_nodes.append((x,y,0.000))
+        nNodesAboveBranch+=1
+    for i in range(nn):
+        y-=h
+        root_nodes.append((x,y, 0.000))
+        nNodesAboveBranch+=1
+        
+    vtkSegmentId = 0
+    for xProx, xDist in zip(root_nodes[:-2], root_nodes[1:-1]):
+
+        RootData.append([vtkSegmentId, xProx[0], xProx[1], xProx[2], xDist[0], xDist[1], xDist[2]])
+        RootConn.append([vtkSegmentId, vtkSegmentId-1, vtkSegmentId+1])
+        vtkSegmentId+=1
+
+    xProx, xDist = root_nodes[-2], root_nodes[-1]
+    RootData.append([vtkSegmentId, xProx[0], xProx[1], xProx[2], xDist[0], xDist[1], xDist[2]])
+    RootConn.append([vtkSegmentId, vtkSegmentId-1])
+    vtkSegmentId+=1
+    RootConn[0]  = [0, -1, 1, vtkSegmentId]
+    RootConn[1]  = [1, 0, 2]
+
+    ### The branch going under the square
+    x,y = root_nodes[1][:-1]
+    root_nodes = [(x,y, 0.000)]
+
+    nn = n-nn
+    for i in range(nn):
+        y-=h
+        root_nodes.append((x,y,0.000))
+    for i in range(n+1):
+        x+=h
+        root_nodes.append((x,y,0.000))
+    for i in range(nn-1):
+        y+=h
+        root_nodes.append((x,y, 0.000))
+
+    xProx, xDist = root_nodes[0], root_nodes[1]
+    RootData.append([vtkSegmentId, xProx[0], xProx[1], xProx[2], xDist[0], xDist[1], xDist[2]])        
+    RootConn.append([vtkSegmentId, RootConn[0][0], vtkSegmentId+1])
+    vtkSegmentId+=1
+
+    for xProx, xDist in zip(root_nodes[1:-1], root_nodes[2:]):
+        RootData.append([vtkSegmentId, xProx[0], xProx[1], xProx[2], xDist[0], xDist[1], xDist[2]])        
+        RootConn.append([vtkSegmentId, vtkSegmentId-1, vtkSegmentId+1])
+        vtkSegmentId+=1
+
+    RootConn[-1] = [vtkSegmentId-1, vtkSegmentId-2]
+    return RootData, RootConn, vtkSegmentId
+
+
+def LinkToRootTree(VesselsData, VesselsConn, RootData, RootConn):
+    # Needs to return the closest neigbhours to which connect the end points of both tree (root and segmented)
+    # while not assigning a vessel from the root tree to more than one segmented vessel
+    def dist(xDist, xProx):
+        return sum([(x-y)**2 for x,y in zip(xDist, xProx)])**.5
+
+    links = []
+    UsedSegments = [0]
+    
+    for i,vessel_segment in enumerate(VesselsData):
+        SegmentId = vessel_segment[0]
+        if VesselsConn[i][1] == -2: # Identifies a vessel that needs to be connected
+
+            xDist = vessel_segment[4:7]
+            distToxDist = []
+            
+            for segment in RootData:
+                if not segment[0] in UsedSegments:
+                    xProx = segment[4:7]
+                    distToxDist.append((dist(xDist, xProx), segment[0]))
+
+            distToxDist = sorted(distToxDist, key=itemgetter(0))
+            IdClosestNeighbour = distToxDist[0][1]
+            links.append([i, IdClosestNeighbour])
+            UsedSegments.append(IdClosestNeighbour)
+
+    # Update the connectivity tables and add the connecting segments in the data tables
+    vtkSegmentId = VesselsData[-1][0]+1
+    for IdVessel, IdRoot in links:
+        ## Add the segments data to the root tree
+        xProx = RootData[IdRoot][4:7]
+        xDist = VesselsData[IdVessel][1:4]
+        RootData.append([vtkSegmentId, xProx[0], xProx[1], xProx[2], xDist[0], xDist[1], xDist[2]])
+
+        ## Update the connectivity tables
+        VesselsConn[IdVessel][1] = vtkSegmentId
+        RootConn[IdRoot].append(vtkSegmentId)
+        RootConn.append([vtkSegmentId, IdRoot, VesselsConn[IdVessel][0]])
+        vtkSegmentId +=1
+
+    return    
+
+def SaveTree(filename, VesselsData, VesselsConn, RootData, RootConn):
+    Radius = 0.04
+    # global tree information
+    xPerf = RootData[0][1:4]
+    qProx = 10.00
+    psiFactor = 6.25e-07
+    dp        = 3.04e+07
+    nTerms    = 2
+    for segment in VesselsData:
+        if len(segment) == 2:
+            nTerms+=1
+    refPressure = 9.80
+    pointCounter = 0
+    rootRadius   = Radius
+    variationTolerance = 1e-05
+
+    with open(filename, 'w') as f:
+        f.write('*Tree\n')
+        f.write(f'{xPerf[0]} {xPerf[1]} {xPerf[2]} {qProx} {psiFactor} {dp} {nTerms} {refPressure} {pointCounter} {rootRadius} {variationTolerance}\n\n')
+        
+        # Vessel information
+        unused         = 0.0
+        qReservedFrac  = 0.0
+        branchingMode  = 1      # BRANCHING_MODE {NO_BRANCHING, RIGID_PARENT, DEFORMABLE_PARENT, DISTAL_BRANCHING, ONLY_AT_PARENT_HOTSPOTS}
+        vesselFunction = 1      # VESSEL_FUNCTION { DISTRIBUTION, PERFORATOR, TRANSPORT }
+        stage          = 0      # 0 for the 'root', 1 for the segmented vessels
+        
+        f.write('*Vessels\n')
+        f.write(str(len(VesselsData) + len(RootData)) + '\n')
+        for segment in RootData:
+            vtkSegmentId = segment[0]
+            xProx = segment[1:4]
+            xDist = segment[4:7]
+            f.write(f'{vtkSegmentId:d} {xProx[0]} {xProx[1]} {xProx[2]} {xDist[0]} {xDist[1]} {xDist[2]} {unused} {unused} {unused} {qReservedFrac} {branchingMode:d} {Radius} {unused} {unused} {unused} {unused} {vesselFunction} {unused} {unused} {stage:d}\n')
+
+        stage+=1
+        for segment in VesselsData:
+            vtkSegmentId = segment[0]
+            xProx = segment[1:4]
+            xDist = segment[4:7]
+            
+            f.write(f'{vtkSegmentId:d} {xProx[0]} {xProx[1]} {xProx[2]} {xDist[0]} {xDist[1]} {xDist[2]} {unused} {unused} {unused} {qReservedFrac} {branchingMode:d} {Radius} {unused} {unused} {unused} {unused} {vesselFunction} {unused} {unused} {stage:d}\n')
+
+        f.write('\n*Connectivity\n')
+        for segment in RootConn:
+            s = ' '.join(str(Id) for Id in segment)
+            f.write(s+'\n')
+        for segment in VesselsConn:
+            s = ' '.join(str(Id) for Id in segment)
+            f.write(s+'\n')
+
+
---
+++ virtual_angiogenesis/MultiStageWithResetOfDlim/Python/WriteCCOFile.py
@@ -0,0 +1,58 @@
+import glob
+import csv
+import os
+from utils import CreateRootTree, LinkToRootTree, SaveTree
+
+FOV    = 0.3                    # Size of the field of view. Shifts the FAZ to (x,y,z)=(0,0,0)
+Radius = 0.004                  # Fixed radius for all vessels
+                                # TODO: add radius for each vessel
+NVessels = 0                    # Number of vessels
+NSegments = 0                   # Total number of segments in the segmented vessels
+Vessels  = []                   # Container for vessels [(start_node, end_node)]
+
+nodes = []                      # Container for nodes [(x0,y0,z0), (x1,y1,z1)...]
+connectivity = []               # Container for connectivity 
+
+## Save each segments data to a container. Each entry is a line in the cco file
+VesselsData = []
+VesselsConn = []
+vtkSegmentId = 0
+unused         = 0.0
+qReservedFrac  = 0.0
+branchingMode  = 2      # BRANCHING_MODE {NO_BRANCHING, RIGID_PARENT, DEFORMABLE_PARENT, DISTAL_BRANCHING, ONLY_AT_PARENT_HOTSPOTS}
+vesselFunction = 1      # VESSEL_FUNCTION { DISTRIBUTION, PERFORATOR, TRANSPORT }
+stage          = 2      
+
+
+# Create a root tree
+n = 6
+RootData, RootConn, vtkSegmentId = CreateRootTree(FOV, Radius, n)
+
+# Read data from files
+for filename in glob.iglob('./Vessel*'):
+    NVessels +=1
+    with open(filename) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        next(csv_reader)
+        row = next(csv_reader)
+        xProx = (float(row[1])-FOV/2., float(row[2])-FOV/2., 0.000)
+        isFirstSegment = True
+        for row in csv_reader:
+            xDist = (float(row[1])-FOV/2., float(row[2])-FOV/2., 0.000) # x,y,z
+
+            ## Store vessel data
+            VesselsData.append([vtkSegmentId, xProx[0], xProx[1], xProx[2], xDist[0], xDist[1], xDist[2]])
+
+            ## Store vessel connectivity
+            if isFirstSegment:
+                VesselsConn.append([vtkSegmentId, -2]) # -2 identifies a segment that needs a parent in the root tree
+                isFirstSegment=False
+            else:
+                VesselsConn[-1].append(vtkSegmentId)
+                VesselsConn.append([vtkSegmentId, vtkSegmentId-1])
+            vtkSegmentId+=1
+            xProx = xDist
+
+LinkToRootTree(VesselsData, VesselsConn, RootData, RootConn)
+SaveTree('RootTreeFromImage.cco', VesselsData, VesselsConn, RootData, RootConn)
+
