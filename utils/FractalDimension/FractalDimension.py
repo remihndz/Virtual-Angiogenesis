4c21625d836071408b2631296309f0945e0f3fa0
remihndz <heremi@orange.fr>
2022-02-22 16:44:18 +0000

    Minor changes.

--- utils/FractalDimension/FractalDimension.py
+++ utils/FractalDimension/FractalDimension.py
@@ -4,29 +4,33 @@ import sys
 import numpy as np
 from matplotlib.pyplot import imread
 import matplotlib.pyplot as plt
+ 
+def FractalDimension(img, threshold=15.0):
 
-def fractal_dimension(Z, threshold=10):
-    # Only for 2d image
-    assert(len(Z.shape) == 2)
-    
+    # Only for grayscale images
+    if len(img.shape)>2:
+    R, G, B = img[:,:,0], img[:,:,1], img[:,:,2]
+    imgGray = 0.2989 * R + 0.5870 * G + 0.1140 * B
+        
+
     # From https://github.com/rougier/numpy-100 (#87)
-    def boxcount(Z, k):
+    def boxcount(img, k):
         S = np.add.reduceat(
-            np.add.reduceat(Z, np.arange(0, Z.shape[0], k), axis=0),
-            np.arange(0, Z.shape[1], k), axis=1)
+            np.add.reduceat(img, np.arange(0, img.shape[0], k), axis=0),
+            np.arange(0, img.shape[1], k), axis=1)
         
         # We count non-empty (0) and non-full boxes (k*k)
         return len(np.where((S > 0) & (S < k*k))[0])
     
-    # Transform Z into a binary array
-    Z = (Z < threshold)
+    # Transform img into a binary array
+    img = (img < threshold)
     plt.figure(1)
-    plt.imshow(Z, cmap=plt.cm.gray_r)
+    plt.imshow(img, cmap=plt.cm.gray_r)
     plt.title(f'Binarized image with threshold {threshold:1.2f}')
     plt.legend()
     
     # Minimal dimension of image
-    p = min(Z.shape)
+    p = min(img.shape)
 
     # Greatest power of 2 less than or equal to p
     n = 2**np.floor(np.log(p)/np.log(2))
@@ -40,7 +44,7 @@ def fractal_dimension(Z, threshold=10):
     # Actual box counting with decreasing size
     counts = []
     for size in sizes:
-        counts.append(boxcount(Z, size))
+        counts.append(boxcount(img, size))
         
     # Fit the successive log(sizes) with log (counts)
     coeffs = np.polyfit(np.log(sizes), np.log(counts), 1)
@@ -57,10 +61,10 @@ def fractal_dimension(Z, threshold=10):
     return
 
 
-I = imread(str(sys.argv[1]))
+img = imread(str(sys.argv[1]))
 if len(sys.argv)==3:
     threshold = float(sys.argv[2])
 else:
-    threshold = 50
+    threshold = 20
 
-fractal_dimension(I, threshold)
+FractalDimension(img, threshold=threshold)
