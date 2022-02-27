68e86b9072d9e73502d0ef2c61769222cb501d0a
remihndz <heremi@orange.fr>
2022-02-27 21:09:28 +0000

    Fixed the script

--- virtual_angiogenesis/10Simulations3500-200.sh
+++ virtual_angiogenesis/10Simulations3500-200.sh
@@ -4,9 +4,9 @@
 rm LogFiles/*
 for P in {1..10} ;
 do
-    mkdir ../perfused_geometries/sim${P} &&
-	time ./dcco_svp ConfigFiles/Baseline.conf > LogFiles/sim${P}.log
-    mv ../perfused_geometries/Baseline.* ./sim${P}/
+    mkdir ../perfused_geometries/sim${P} 
+    time ./dcco_svp ConfigFiles/Baseline.conf > LogFiles/sim${P}.log 
+    mv ../perfused_geometries/Baseline.* ../perfused_geometries/sim${P}/ 
     cp LogFiles/sim${P}.log ../perfused_geometries/sim${P}/;
 done
 
