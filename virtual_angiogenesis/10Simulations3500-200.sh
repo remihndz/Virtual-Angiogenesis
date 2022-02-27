8f7ab6feae88e5140f2375e4d1df372bd7096b4d
remihndz <heremi@orange.fr>
2022-02-27 20:51:20 +0000

    Added a script to run 10 simulations. The simulations are expected to give a VD around the right value (30~40%).

---
+++ virtual_angiogenesis/10Simulations3500-200.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Runs the same simulation with three different 'minimum bifurcation angle' parameter
+rm LogFiles/*
+for P in {1..10} ;
+do
+    mkdir ../perfused_geometries/sim${P} &&
+	time ./dcco_svp ConfigFiles/Baseline.conf > LogFiles/sim${P}.log
+    mv ../perfused_geometries/Baseline.* ./sim${P}/
+    cp LogFiles/sim${P}.log ../perfused_geometries/sim${P}/;
+done
+
+echo "Simulations terminated. Log files are in 'Virtual-Angiogenesis/LogFiles/', results in 'Virtual-Angiogenesis/perfused-geometries/sim{x}/'."
