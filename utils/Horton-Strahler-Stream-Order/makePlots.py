Corrected some units error

utils/Horton-Strahler-Stream-Order/makePlots.py
@@ -39,11 +39,11 @@ orderDistribution = np.zeros((maxOrder,))
 
 # Load the data
 for Id, radius, length, flow, stage, order in treesData:
-    r,l = radius*1e4, length*1e4 # Convert to microns
+    r,l = radius*1e3, length*1e3 # Convert to microns
     i,j = int(order)-1, int(Id)
-    dataRadius[i, j] = radius
-    dataLength[i, j] = length
-    dataAspectRatio[i, j] = length/radius
+    dataRadius[i, j] = r
+    dataLength[i, j] = l
+    dataAspectRatio[i, j] = l/r
     orderDistribution[i] += 1
 
 orderDistribution /= len(sys.argv[1:])

utils/Horton-Strahler-Stream-Order/utils.py
@@ -24,7 +24,7 @@ def ReadTree(ccoFile):
             row = (f.readline()).split() # Split all columns in a list
             Id, xProx, xDist, r, q, stage = int(row[0]), row[1:4], row[4:7], float(row[12]), float(row[10]), int(row[-1])
             l = sum([(float(a)-float(b))**2 for a,b in zip(xProx, xDist)])**.5
-            treeData.append([Id, r*1e4, l*1e4, q, stage]) # Convert to mm
+            treeData.append([Id, r*10, l*10, q, stage]) # Convert to mm
         
         row = f.readline()
         print('Reading', f.readline().strip())
@@ -129,7 +129,7 @@ def PlotTreeStatistics(orderedTree, outputImageFile=None):
     orderDistribution = np.zeros((maxOrder,))
     
     for Id, radius, length, flow, stage, order in orderedTree:
-        r,l = radius*1e4, length*1e4 # Convert to microns
+        r,l = radius*1e5, length*1e5 # Convert to microns
         i,j = int(order)-1, int(Id)
         dataRadius[i, j] = radius
         dataLength[i, j] = length