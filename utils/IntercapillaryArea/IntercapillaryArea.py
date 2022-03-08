42735e7452f4f7e874e9a35f7fa41defba774953
Remi <heremi@orange.fr>
2022-03-08 20:24:32 +0000

    Now saves the data in .dat

--- utils/IntercapillaryArea/IntercapillaryArea.py
+++ utils/IntercapillaryArea/IntercapillaryArea.py
@@ -1,6 +1,8 @@
 import sys
 from utils import IntercapillaryDistanceMap
-from numpy import array
+from numpy import array, savetxt
+import numpy as np
+
 
 assert(len(sys.argv)>1)
 
@@ -20,5 +22,10 @@ mean = array(means).mean()
 std  = sum(variances)**.5
 print(u'Mean\u00B1std of intercapillary distance out of', count, 'images:', mean, u"\u00B1", std)
 
-
-
+variances = array(variances)**.5
+variances.reshape((-1,))
+means = array(means)
+data = np.zeros((means.shape[0],2))
+data[:,0] = means
+data[:,1] = variances
+savetxt('MeanAndStdOfICD.dat', data)
